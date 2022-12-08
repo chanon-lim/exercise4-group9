48986c110e9759d8326a4465297dd6f08d459492
chanon-lim <s1f102100041@iniad.org>
2022-12-08 16:08:14 +0900

    Create models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+    )
+    image = models.ImageField(blank=True)
+    bio = models.CharField(max_length=240, blank=True)
+
+    def __str__(self):
+        return self.user.get_username()
+
+
+class Label(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    class Meta:
+        ordering = ["-updated_on"]
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, null=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now= True)
+    like = models.IntegerField()
+    dislike = models.IntegerField()
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    labels = models.ManyToManyField(Label, blank=True)
+
+class Comment(models.Model):
+    class Meta:
+        ordering = ["-created_on"]
+
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now= True)
+    like = models.IntegerField()
+    dislike = models.IntegerField()
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
