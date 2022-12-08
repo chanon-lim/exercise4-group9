978cd83a7f8b6bac3ba4e6233182103460637757
chanon-lim <s1f102100041@iniad.org>
2022-12-08 16:17:07 +0900

    test html

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Profile, Label, Post, Comment
 
 # Create your views here.
 def index(request):
     return render(request, 'forum/index.html')
+
+def post_list(request):
+    posts = Post.objects.order_by('-updated_on')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'forum/post_list.html', context)
+
+def post(request, slug):
+    post = Post.objects.get(slug=slug)
+    comments = Comment.objects
+    context = {
+        'post': post,
+    }
+    return render(request, 'forum/post.html', context)
